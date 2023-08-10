d26b8d486426bbfb0aa7212b175d5c689f8bbb36
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2023-08-10 23:03:59 +0900

    UI 컴포넌트 기초 구현
    아직 세부사항은 구현하지 못했음.

--- components/problems/Problem.js
+++ components/problems/Problem.js
@@ -1,4 +1,6 @@
 import { Fragment } from "react";
+import Button from "../../UI/Button";
+import Input from "../../UI/Input";
 
 function Problem(props) {
     console.log(props.text);
@@ -8,6 +10,10 @@ function Problem(props) {
         <p>{props.nowIdx} / {props.totalNum}</p>
         <h1>{props.text}</h1>
         <h3>{props.name}</h3>
+      </div>
+      <div>
+        <Input id="text"></Input>
+        <Button>Submit</Button>
       </div>
     </Fragment>
   );
