79104a7c1b1c4b3e5df04723817b9f4d0470cce6
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2023-08-21 22:28:10 +0900

    끝 페이지 기초 구현

---
+++ components/EndPage/Endpage.js
@@ -0,0 +1,5 @@
+function EndPage() {
+    return <h1>This is End.</h1>
+}
+
+export default EndPage;
---
+++ pages/end.js
@@ -0,0 +1,8 @@
+import EndPage from "../components/EndPage/Endpage"
+
+//domain/end
+const endPage = () =>{
+    return <EndPage />;
+}
+
+export default endPage;
