79a5adb424cce41647c522681dcbb606322e2388
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2023-08-27 00:14:27 +0900

    임시 showHiragana  컴포넌트 생성

--- components/problems/showHiragana.js
+++ components/problems/showHiragana.js
@@ -1,5 +1,15 @@
-function ShowHiragana() {
-    return <p>This is the showHiragana!</p>
+import { useState } from "react";
+import ProblemList from "./ProblemList";
+
+function ShowHiragana(props) {
+    const [isStart, setIsStart] = useState(false);
+    const onClickHandler = () => {
+        setIsStart((isStart) => !isStart);
+    }
+    return <div>
+        <button onClick={onClickHandler}>go</button>
+        {isStart && <ProblemList problems = {props.problems} />}
+    </div>
 }
 
 export default ShowHiragana;    
