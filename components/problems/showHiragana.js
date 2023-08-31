b0b881a3a34ad62f58f0fa33b3bd25eee0f74905
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2023-08-31 21:48:13 +0900

    showHiragana의 버튼 없앰

--- components/problems/showHiragana.js
+++ components/problems/showHiragana.js
@@ -4,15 +4,9 @@ import { useDispatch } from "react-redux";
 import { counterActions } from "../../store";
 
 function ShowHiragana(props) {
-  const [isStart, setIsStart] = useState(false);
   const dispatch = useDispatch();
 
-  const onClickHandler = () => {
-    setIsStart((isStart) => !isStart);
-  };
-
   useEffect(() => {
-    if (isStart) {
       const currentDatetime = new Date();
       const hours = currentDatetime.getHours();
       const minutes = currentDatetime.getMinutes();
@@ -22,13 +16,12 @@ function ShowHiragana(props) {
       const startTime = hours * 3600 + minutes * 60 + seconds + ms / 1000;
       dispatch(counterActions.setStartTime(startTime));
       dispatch(counterActions.setCounter(0));
-    }
-  }, [isStart]);
+    
+  }, []);
 
   return (
     <div>
-      {!isStart && <button onClick={onClickHandler}>go</button>}
-      {isStart && <ProblemList problems={props.problems} />}
+      <ProblemList problems={props.problems} />
     </div>
   );
 }
